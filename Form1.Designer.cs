dad6d530904b52f0335829c4693cf30ca1fb4e37
j-mrosa <78103693+j-mrosa@users.noreply.github.com>
2021-04-27 09:01:32 -0300

    adding files

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Practice_4_tabuada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstTabuada = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.nudNum = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose a number:";
+            // 
+            // lstTabuada
+            // 
+            this.lstTabuada.FormattingEnabled = true;
+            this.lstTabuada.ItemHeight = 25;
+            this.lstTabuada.Location = new System.Drawing.Point(322, 167);
+            this.lstTabuada.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.lstTabuada.Name = "lstTabuada";
+            this.lstTabuada.Size = new System.Drawing.Size(194, 304);
+            this.lstTabuada.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 167);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Multiplication Table:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalcular.Location = new System.Drawing.Point(556, 77);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(150, 44);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "&Calculate";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // nudNum
+            // 
+            this.nudNum.Location = new System.Drawing.Point(356, 85);
+            this.nudNum.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.nudNum.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNum.Name = "nudNum";
+            this.nudNum.Size = new System.Drawing.Size(72, 30);
+            this.nudNum.TabIndex = 5;
+            this.nudNum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalcular;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 542);
+            this.Controls.Add(this.nudNum);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstTabuada);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multiplication Tables";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstTabuada;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.NumericUpDown nudNum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_4_tabuada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Random rnd = new Random();
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            lstTabuada.Items.Clear();
+
+            int num = Convert.ToInt32(nudNum.Value);
+
+            for (int i = 1; i <= 10; i++)
+                lstTabuada.Items.Add(i + " x " + num + " = " + num * i);
+
+            Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+
+            BackColor = randomColor;
+        }
+
+
+    }
+}
